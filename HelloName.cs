da4a90b3190e39ae7e932a89794c153b0d3bae9c
Sm4rtD3v <166156036+Sm4rtD3v@users.noreply.github.com>
2025-03-19 21:18:31 +0100

    Add files via upload

---
+++ HelloName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine()!;
+            if (string.IsNullOrWhiteSpace(myFirstName))
+            {
+                myFirstName = "John";
+            }
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine()!;
+            if (string.IsNullOrWhiteSpace(myLastName))
+            {
+                myLastName = "Doe";
+            }
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + "!");
+        }
+    }
+}
