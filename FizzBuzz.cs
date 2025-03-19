98616eb96d850113c095790bbfe4c14c997e7c2e
Sm4rtD3v <166156036+Sm4rtD3v@users.noreply.github.com>
2025-03-19 21:20:39 +0100

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputString = Console.ReadLine().Split();
+            int[] input = new int[inputString.Length];
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                input[i] = int.Parse(inputString[i]);
+            }
+
+            for (int i = 1; i <= input[2]; i++)
+            {
+                if (i % input[0] == 0 && i % input[1] == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % input[0] == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % input[1] == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
