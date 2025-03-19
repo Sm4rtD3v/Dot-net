8237bc4c4b86b02eb419ff3af70318481bbfa429
Sm4rtD3v <166156036+Sm4rtD3v@users.noreply.github.com>
2025-03-19 21:19:31 +0100

    Add files via upload

---
+++ Decisions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giveaway time!");
+            Console.Write("Choose a number: ");
+            string? userChoice = Console.ReadLine();
+            string message = "";
+            if (userChoice == "1")
+                message = "won a car";
+            else if (userChoice == "2")
+                message = "won a boat";
+            else if (userChoice == "3")
+                message = "won a plane";
+            else
+                message = "didn't win anything";
+            Console.WriteLine("You {0}!", message);
+        }
+    }
+}
