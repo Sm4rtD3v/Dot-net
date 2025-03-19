57329bb630fa6b0d369960394a0d89a9797e961c
Sm4rtD3v <166156036+Sm4rtD3v@users.noreply.github.com>
2025-03-19 21:21:07 +0100

    Add files via upload

---
+++ ColdPuterScience.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ColdPuterScience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] inputString = Console.ReadLine().Split();
+            int[] input = new int[inputString.Length];
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                input[i] = int.Parse(inputString[i]);
+            }
+
+            int negNbrCount = 0;
+            foreach (int temperature in input)
+            {
+                if (temperature < 0)
+                {
+                    negNbrCount++;
+                }
+            }
+            Console.WriteLine(negNbrCount);
+        }
+    }
+}
