7112a613087dcd47626a88eebfa32a8a9914eb4a
Sm4rtD3v <166156036+Sm4rtD3v@users.noreply.github.com>
2025-03-19 21:20:01 +0100

    Add files via upload

---
+++ Iterations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 50; i++)
+            {
+                bool isPrime = true;
+                for (int x = 2; x <= Math.Sqrt(i); x++)
+                {
+                    if (i % x == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {           
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
