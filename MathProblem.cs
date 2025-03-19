5a9a0a311ecf4d5a23dd26ac3ceeffc9ffea26f4
Sm4rtD3v <166156036+Sm4rtD3v@users.noreply.github.com>
2025-03-19 21:18:48 +0100

    Add files via upload

---
+++ MathProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MathProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine() ?? "0");
+            int y = x + 3;
+            Console.WriteLine(y);
+        }
+    }
+}
